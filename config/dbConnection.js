1e5087bd4dd30923625c83983fc9d576f8e69b59
Joao Calado <joao1823@hotmail.com>
2021-03-11 10:02:05 -0300

    Connection com C  maiúsculo.

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,19 +1,19 @@
 const { Pool, Client } = require("pg");
 
-const postGres = function(){
-   
-  const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "projeto_sangriaprovento",
-    password: "postgres123",
-    port: "5432"
-  });
+const postGres = function() {
+
+    const pool = new Pool({
+        user: "postgres",
+        host: "localhost",
+        database: "projeto_sangriaprovento",
+        password: "postgres123",
+        port: "5432"
+    });
 
     return pool
 }
 
-module.exports = function(){
-		
+module.exports = function() {
+
     return postGres
 }
--- config/server.js
+++ config/server.js
@@ -8,13 +8,13 @@ app.set('views', './app/views');
 
 
 app.use(express.static('./app/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 consign()
-	.include('app/routes')
-	.then('config/dbconnection.js')
-	.then('app/models')
-	.then('app/controllers')
-	.into(app);
+    .include('app/routes')
+    .then('config/dbConnection.js')
+    .then('app/models')
+    .then('app/controllers')
+    .into(app);
 
 module.exports = app;
