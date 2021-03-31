1654cdb9036fe0ddbbb9a6c38d4e36e085b72a36
Guilherme Gomes leal <guilhermegomesleal@hotmail.com>
2021-03-31 10:31:24 -0300

    muitas modificações

--- app/controllers/sangprovController.js
+++ app/controllers/sangprovController.js
@@ -1,9 +1,18 @@
 module.exports.salvarSangria = function(app, req ,res){
 
-    let form = req.body;
- 
-     console.log(form)
+    let form = req.body
 
+    req.assert("valor", "valor não pode ser vazio!!").notEmpty();
+    req.assert("motivo", "motivo não pode ser vazio!!").notEmpty();
+    req.assert("motivo", "motivo deve conter entre 10 e 150 caracteres").len(10, 150)
+
+    const erros = req.validationErrors();
+
+	if(erros){
+
+		res.render("home/principal", {validacao: erros, caixa: {}})
+		return "";
+	}
      res.redirect('/')
     
 }
