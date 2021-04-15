be124a62705c8112976170b14a4b6db8c15d685f
Joao Calado <joao1823@hotmail.com>
2021-04-15 14:35:56 -0300

    tried1

--- cypress/integration/teste.spec.js
+++ cypress/integration/teste.spec.js
@@ -1,5 +1,24 @@
-describe('caixa aberto',()=>{
-    it('Test true equal true',()=>{
-        expect(true).to.equal(true)
+// describe('Caixa aberto',()=>{
+//     it('Verificar se o caixa está aberto',()=>{
+//         expect(true).to.equal(true)
+//     })
+// })
+
+// get vai buscar qualquer componente com determinado name
+// enquanto contains vai buscar  em tela
+
+// describe('Caixa aberto',()=>{
+//     it('Verificar se o caixa está aberto',()=>{
+//         cy.visit('http://localhost:1024')
+//         cy.contains('Sangria')
+//     })
+// })
+
+beforeEach(()=> cy.visit('/'))
+
+describe('Caixa aberto',()=>{
+    it('Verificar se o caixa está aberto',()=>{
+        
+        cy.get('input[name=valor]')
     })
 })
