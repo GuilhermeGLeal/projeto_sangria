992f3d8af1165b5e5db0a4fd1a7af51a9502b60e
Joao Calado <joao1823@hotmail.com>
2021-05-27 09:54:41 -0300

    commito

--- cypress/integration/teste2.spec.js
+++ cypress/integration/teste2.spec.js
@@ -1,7 +1,6 @@
 beforeEach(()=> cy.visit('/'))
 
 // cypress test using fixtures
-
 function processData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(',');
@@ -10,7 +9,7 @@ function processData(allText) {
     for (var i=1; i<allTextLines.length; i++) 
     {
         var data = allTextLines[i].split(',');
-        if (data.length == headers.length) 
+        if (data.length == headers.length)
         {
             var tarr = [];
             for (var j=0; j<headers.length; j++) 
@@ -28,21 +27,19 @@ describe('Teste',()=>{
 
     let CSV;
     let CSVA=[];
-    beforeEach(()=>{
+    before(()=>{
         cy.fixture('teste.csv').then(
         (Exemplo)=>
         {
             cy.intercept('GET','/teste.csv',Exemplo);
             CSV=Exemplo;
+            CSVA=processData(CSV);
         })
-        
     })
 
+    CSVA.forEach
     it('',()=>
     {     
-        CSVA=processData(CSV);
-        console.log(CSVA)
-
         CSVA.forEach(element => {
             let caixa = element[0].split(":")
             let saldo = element[1].split(":")
@@ -50,23 +47,26 @@ describe('Teste',()=>{
             let tipo = element[3].split(":")
             let motivo = element[4].split(":")
             let resultado  = element[5].split(":")
-
+    
             // caixa aberto
             if(caixa[1] === "VERDADEIRO")
                 cy.get('input[name=caixa_fechamento]').should("have.value", "")
-
+    
             // selecionar o tipo da transação
             cy.get('select[name=tipo]').select(tipo[1])
-
+    
             // verificar o motivo
-            cy.get('textarea').type(motivo[1]).clear()
+            cy.get('textarea').type(motivo[1])
             // assert.isNotEmpty('textarea', 'vazio')
-
+    
             // escrever o 'valor' no devido campo
-            cy.get('input[name=valor]').type(parseFloat(valor[1])).clear()
-
+            cy.get('input[name=valor]').type(valor[1])
+    
             // escrever o 'saldo' no devido campo
-            cy.get('input[name=caixa_valorfinal]').type(parseFloat(saldo[1])).clear()
+            cy.get('input[name=caixa_valorfinal]').type(saldo[1])
+    
+            // verificar valor nulo
+            expect(saldo[1]).to.not.equal("-")
             
             if(tipo[1] === "Sangria") {
                 // sangria 1
@@ -75,11 +75,16 @@ describe('Teste',()=>{
                 //     parseFloat(saldo[1]),
                 //     'Valor da Sangria não pode ser maior que o Saldo Atual do caixa'
                 // )
-
+    
                 // sangria 2
                 // cy.get('input[name=caixa_valorfinal]')
                 // expect(parseFloat(saldo[1])).to.be.greaterThan(parseFloat(valor[1]))
             }
+
+            // limpar campos após testes
+            cy.get('textarea').clear()
+            cy.get('input[name=valor]').clear()
+            cy.get('input[name=caixa_valorfinal]').clear()
         });
     })
 })
